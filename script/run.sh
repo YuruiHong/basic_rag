bc34d66e5f044ba16795a5dad6354c1b73647a11
Yurui Hong <yuruihong02@outlook.com>
2025-03-05 23:05:16 +0800

    fix: create log dir

--- script/run.sh
+++ script/run.sh
@@ -1,2 +1,3 @@
+mkdir -p logs
 nohup python src/server/main_service_online.py llm_model >> logs/llm_model.log 2>&1 &
 nohup python src/server/main_service_online.py searcher >> logs/searcher.log 2>&1 &
